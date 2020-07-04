fb8d020dfb73af2a4ec69fca8e55b7b0c6b0e529
CaseyInHaengsin <casey.rowley89@gmail.com>
2020-07-04 15:48:55 -0600

    final commit for the day.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,13 +8,19 @@ class SearchBar extends React.Component{
         console.log(event.target.value)
 
     }
+    
+    
     render(){
         return (
             <div className="ui segment">
             <form className="ui form">
                 <div className="field">
                     <label>Image Search</label>
-                    <input type="text" onChange={this.onInputChange}/>
+                    <input 
+                        type="text" 
+                        onChange={this.onInputChange}
+                        />
+
                 </div>
             </form>
             </div>
