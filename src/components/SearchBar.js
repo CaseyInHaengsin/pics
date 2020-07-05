e17214143324f8495ec046460e1599933f9bb067
CaseyInHaengsin <casey.rowley89@gmail.com>
2020-07-05 14:44:59 -0600

    using an arrow function so that the state is binded..

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,22 +3,28 @@ import ReactDOM from 'react-dom';
 
 
 class SearchBar extends React.Component{
+    
 
     state = {
         term: ""
     };
-    
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.term);
+
+    }
     
     render(){
         return (
             <div className="ui segment">
-            <form className="ui form">
+            <form onSubmit={this.onFormSubmit} className="ui form">
                 <div className="field">
                     <label>Image Search</label>
                     <input 
                         type="text"
                         value={this.state.term}
-                        onChange={(e) => this.setState({term: e.target.value.toUpperCase()})}
+                        onChange={(e) => this.setState({term: e.target.value})}
                         />
 
                 </div>
