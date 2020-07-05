4e318ea86d5fd5ba81ba9999c27a4dcafe610153
CaseyInHaengsin <casey.rowley89@gmail.com>
2020-07-05 14:53:40 -0600

    This commit demonstrates passing data back up to the app component.. oh yeah

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 import SearchBar from './SearchBar';
 
 
+class App extends React.Component {
 
-const App = () => {
-    return (
-        <div className="ui container" style={{marginTop: '10px'}}>
-            <SearchBar />
-        </div>
-        )
+    onSearchSubmit(term){
+        console.log(term);
+
+    }
+    render(){
+        return (
+            <div className="ui container" style={{marginTop: '10px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        );
+    }
 }
 
 export default App;
